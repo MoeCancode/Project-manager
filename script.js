6d6336f3c0462cc12d2338f97185f815c18cf1d8
Samuel Mandel <slman.760@gmail.com>
2022-04-06 12:36:43 -0700

    added modal and form selction

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-var displayTimer = document.querySelector("#timer");
-var time = moment();
 
+$('#myModal').on('shown.bs.modal', function () {
+    $('#myInput').trigger('focus')
+  })
 
-displayTimer.moment();
-
+var time; 
 var inter = setInterval(() => {
-    
+    time = moment().format('H:mm:ss');
+    $("#timer").text(time)
 }, 1000);
+console.log(inter)
